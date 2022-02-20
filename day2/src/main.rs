a9df3f6df01b6e13ece7899e0fb5c327a6705614
Brad Barrows <bbarrows@LTBrw.local>
2022-02-19 22:38:23 -0800

    day 2 part 1 is done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{BufReader, Lines};
+
+struct Command {
+    cmd_str: String,
+    len: u64,
 }
+
+impl Command {
+    fn new(cmd_str: String, len: u64) -> Command {
+        Command { cmd_str, len }
+    }
+}
+
+fn command_from_str(s: String) -> std::io::Result<Command> {
+    let s_trim = s.trim();
+    let mut s_iter = s_trim.split(" ");
+    let command = s_iter.next().ok_or(std::io::Error::new(std::io::ErrorKind::Other, "No command"))?;
+    let len_str = s_iter.next().ok_or(std::io::Error::new(std::io::ErrorKind::Other, "No len"))?;
+    let len = len_str.parse::<u64>().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("Parse error for len: {}", e)))?;
+    Ok( Command { cmd_str: command.to_string(), len } )
+}
+fn main() -> std::io::Result<()> {
+    // let test_str = r#"down 3
+    // forward 3
+    // up 4"#;
+
+    // for l in test_str.lines() {
+    //     let cmd = command_from_str(l.to_string())?;
+    //     println!("cmd {} {}", cmd.cmd_str, cmd.len);
+    // }
+
+    let mut horizontal: u64 = 0;
+    let mut depth: u64 = 0;
+
+    let f = File::open("input")?;
+    let bf = BufReader::new(f);
+    for (ln_no, l_result) in bf.lines().enumerate() {
+        if l_result.is_err() {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "Error getting line"));
+        }
+        let cmd = command_from_str(l_result.unwrap())?;
+        println!("cmd {} {}", cmd.cmd_str, cmd.len);
+        if cmd.cmd_str == "down" {
+            depth += cmd.len;
+        } else if cmd.cmd_str == "up" {
+            depth -= cmd.len;
+        } else if cmd.cmd_str == "forward" {
+            horizontal += cmd.len;
+        }
+    }
+
+  //let data: StructWithCustomDate = serde_csv::from_str(test_str).unwrap();
+//   println!("{:#?}", data);
+
+    println!("multiplied: {}", horizontal * depth);
+    Ok(())
+ }
